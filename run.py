0a8573f7d37cd5cb9bce90c4cfc209bb85e73858
Charlton Omondi <omondioware@gmail.com>
2022-08-16 22:51:03 +0300

    implement migrate and perform initial migration

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_migrate import Migrate
 
 from config import app_config
+from app.models import db, UserModel, BucketList
 
 #use the config from the environment file as default
 config_name = os.getenv('FLASK_ENV')
@@ -10,5 +11,8 @@ app = Flask(__name__)
 #configure app from an object in config file
 app.config.from_object(app_config['development'])
 
-from app import models, resources, schema
+db.init_app(app)
+
+migrate = Migrate(app, db)
+
 
