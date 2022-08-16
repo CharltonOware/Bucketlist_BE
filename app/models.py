a10cd9b43635400181f3ce312d0fdaa55a10f2e6
Charlton Omondi <omondioware@gmail.com>
2022-08-16 22:49:42 +0300

    Implement the BucketList model

--- app/models.py
+++ app/models.py
@@ -3,7 +3,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
+#Define Usermodel
 class UserModel(db.Model):
+    '''Table to store user information.'''
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -28,3 +30,24 @@ class UserModel(db.Model):
     #Return True if user provided password matches the hash, otherwise return False
     def verify_password(self, password):
         return check_password_hash(self.password, password)
+
+#Define BucketList Model
+class BucketList(db.Model):
+    '''Table to store our bucketlists.'''
+    __tablename__ = 'bucketlists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+            onupdate=db.func.current_timestamp())
+    done = db.Column(db.Boolean, default=False)
+    created_by = db.column(db.Integer, db.ForeignKey("users.id"))
+
+    #Initialize an instance of BucketList model
+    def __init__(self, name, created_by):
+        self.name = name
+        self.created_by = created_by
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
