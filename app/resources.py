82ea4a65c57a8dbf9829239f9e4809243d9d9157
Charlton Omondi <omondioware@gmail.com>
2022-08-17 20:33:09 +0300

    implement user login and user view endpoints

--- app/resources.py
+++ app/resources.py
@@ -23,3 +23,25 @@ class UserRegistration(MethodResource,Resource):
             return {'message': f'New user {new_user.email} created'}
         except:
             return {'message': 'Something went wrong'}, 500
+
+#Define UserLogin endpoint
+class UserLogin(MethodResource, Resource):
+    @use_kwargs(UserResponseSchema, location='json')
+    @marshal_with(UserResponseSchema(), code=200)
+    def post(self, **kwargs):
+        email = kwargs.get('email')
+        current_user = UserModel.find_by_email(email)
+
+        #Abort if credentials don't exist in the DB
+        if not current_user:
+            return {'message': f'User {email} doesn\'t exist'}, 400
+        if current_user.verify_password(kwargs['password']):
+            return {'message': f'Logged in as {current_user.email}'}, 200
+
+#Define view for all user
+class AllUsers(MethodResource, Resource):
+    def get(self):
+        return UserModel.return_all()
+
+    def delete(self):
+        return UserModel.delete_all()
